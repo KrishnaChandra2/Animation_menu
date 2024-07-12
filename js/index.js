15e7285692a64feb0323424a00f41d35fffa4bd3
KrishnaChandra2 <krishnagit11@gmail.com>
2024-07-12 11:34:57 +0600

    Animation javascript
    javascript css html

---
+++ js/index.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".typing", {
+    strings: ["Fronted Developer", "Youtuber", "Web Designer", "Grafics Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 100,
+    loop: true
+})
